d48e513f155dbab93c04564152c57377f8a7d6a6
dev <kohlgtr@gmail.com>
2026-02-22 01:52:57 -0300

    songs

--- assets/js/music.js
+++ assets/js/music.js
@@ -20,15 +20,14 @@ const ECHODOME_TRACKS = [
         src: "assets/audio/echos.mpeg",
         cover: "assets/img/logo.jpeg",
     },
-    /*{
+    {
         id: 3,
-        title: "Untitled 03",
+        title: "Eu Não Queria Sentir Assim",
         artist: "EchoDome",
         duration: "5:01",
-        src: "assets/audio/track03.mp3",
+        src: "assets/audio/eu-nao-queria-sentir-assim.mp3",
         cover: "assets/img/logo.jpeg",
     },
     // Para adicionar mais músicas, copie o bloco acima e cole aqui
     // Lembre de colocar os arquivos .mp3 na pasta assets/audio/
-*/
     ];
